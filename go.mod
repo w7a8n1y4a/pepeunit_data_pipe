91c9e88c546c346d8b0f1b4d199eaf8e16102082
Ivan Serebrennikov <admin@silberworks.com>
2025-05-18 03:06:35 +0300

    feat(jwt): add jwt gen for auth emqx

--- go.mod
+++ go.mod
@@ -4,5 +4,10 @@ go 1.24.3
 
 require (
 	github.com/caarlos0/env v3.5.0+incompatible // indirect
+	github.com/eclipse/paho.mqtt.golang v1.5.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
-
+	
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/caarlos0/env"
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
+	BACKEND_SECRET_KEY      string `env:"BACKEND_SECRET_KEY" envDefault:""`
 	SQLALCHEMY_DATABASE_URL string `env:"SQLALCHEMY_DATABASE_URL" envDefault:""`
 	CLICKHOUSE_DATABASE_URL string `env:"CLICKHOUSE_DATABASE_URL" envDefault:""`
 	REDIS_URL               string `env:"REDIS_URL" envDefault:""`
@@ -17,6 +19,23 @@ type Config struct {
 	MQTT_PORT				int    `env:"MQTT_PORT" envDefault:"1883"`
  	MQTT_KEEPALIVE			int    `env:"MQTT_KEEPALIVE" envDefault:"60"`
 }
+
+func generateToken(secretKey string) (string, error) {
+	claims := jwt.MapClaims{
+		"domain": "example.com",
+		"type":   "Backend",
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	signedToken, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		return "", fmt.Errorf("failed to sign token: %v", err)
+	}
+
+	return signedToken, nil
+}
+
 
 func main() {
 	err := godotenv.Load()
@@ -29,4 +48,13 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Config: %+v\n", cfg)
+	
+	token, err := generateToken(cfg.BACKEND_SECRET_KEY)
+	if err != nil {
+		fmt.Println("Error generating token:", err)
+		return
+	}
+	
+	fmt.Println("Generated JWT token:", token)
+
 }
